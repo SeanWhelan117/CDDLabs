4aca831af6a74c26c893d5e93a707628a3668141
Sean Whelan <seanwhelan@pop-os.localdomain>
2023-03-30 15:40:52 +0100

    Started Barriers

---
+++ Exercise Five: Reusable Barriers/Barrier.cpp
@@ -0,0 +1,76 @@
+// Barrier.cpp --- 
+// 
+// Filename: Barrier.cpp
+// Description: 
+// Author: Joseph
+// Maintainer: 
+// Created: Tue Jan  8 12:14:02 2019 (+0000)
+// Version: 
+// Package-Requires: ()
+// Last-Updated: Tue Jan  8 12:15:21 2019 (+0000)
+//           By: Joseph
+//     Update #: 2
+// URL: 
+// Doc URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
+#include "Semaphore.h"
+#include "Barrier.h"
+
+Barrier::Barrier(int num_threads)
+{
+  numThreads = num_threads;
+  barrier1 = std::make_shared<Semaphore>(0);
+  count = 0;
+}
+
+void Barrier::wait()
+{
+  count = count+1;
+
+    if(count == numThreads)
+      {
+	barrier1->Signal();
+      }
+  barrier1->Wait();
+  barrier1->Signal();
+  count--;
+  
+}
+
+Barrier::~Barrier()
+{
+  //delete barrier1;
+}
+// 
+// Barrier.cpp ends here
